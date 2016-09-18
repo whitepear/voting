91b93ac17b6d7273de6e28bfa6601587ff8d8ae7
John Doe <webdevnorth@gmail.com>
2016-09-19 00:39:36 +0100

    established basic structure for app GET routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// GET /:poll
+router.get('/:poll', function(req, res, next) {
+  if (req.params.poll) {
+		// call database with poll ID provided
+		// pass in info via locals object
+		// render poll template 
+  } else {  	
+  	res.render('index', { title: 'Home' });
+  }
 });
+
+// POST /
+
+// GET /register
+router.get('/register', function(req, res, next) {
+	res.render('register', { title: 'Register' });
+});
+
+// POST /register
+
+// GET /login
+router.get('/login', function(req, res, next) {
+	res.render('login', { title: 'Login' });
+});
+
+// POST /login
+
+
 
 module.exports = router;
