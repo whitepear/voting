7e7f8fdb9b6f09af06b4dc233c573df71626ad0f
John Doe <webdevnorth@gmail.com>
2016-09-21 20:22:23 +0100

    implemented hashing in presave hook (user.js)

--- models/user.js
+++ models/user.js
@@ -20,5 +20,18 @@ var UserSchema = new mongoose.Schema({
 	}
 });
 
+UserSchema.pre('save', function (next) {
+	// mongoose assigns the db object to be inserted to 'this'
+	var user = this;
+
+	bcrypt.hash(user.password, 10, function(err, hash) {
+		if (err) {
+			return next(err);
+		}
+		user.password = hash;
+		next();	
+	});
+});
+
 var User = mongoose.model('User', UserSchema);
 module.exports = User;
