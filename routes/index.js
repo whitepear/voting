1b842774455ec499dc89f13d398c4a67f01b26f0
John Doe <webdevnorth@gmail.com>
2016-09-23 01:42:49 +0100

    created GET /logout

--- routes/index.js
+++ routes/index.js
@@ -79,6 +79,20 @@ router.post('/login', function(req, res, next) {
 	}
 });
 
+// GET /logout
+router.get('/logout', function(req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function(err) {
+      if(err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
+
 // GET /profile
 router.get('/profile', function(req, res, next) {
 	if (!req.session.userId) {
