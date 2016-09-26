236d0112d5e3ee25605ce99c9b8971cf94364b01
John Doe <webdevnorth@gmail.com>
2016-09-26 18:14:53 +0100

    corrected logic bug in loggedIn

--- middleware/index.js
+++ middleware/index.js
@@ -8,7 +8,7 @@ function loggedOut(req, res, next) {
 
 function loggedIn(req, res, next) {
 	// prevent unauthenticated users from accessing a route
-	if (req.session & req.session.userId) {
+	if (req.session && req.session.userId) {
 		next();
 	}
 	var err = new Error('You are not authorized to view this page.');
