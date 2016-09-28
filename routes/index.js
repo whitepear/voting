2c6bf102b54d593ed0c78bdc7a22222e0b45d84e
John Doe <webdevnorth@gmail.com>
2016-09-28 14:43:02 +0100

    added sort to db Find within GET '/'

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var User = require('../models/user.js');
 
 // GET /
 router.get('/', function(req, res, next) {
-	User.find({ polls: { $gt: [] } }, { email: 0, password: 0, __v: 0 }, function(err, docs) {
+	User.find({ polls: { $gt: [] } }, { email: 0, password: 0, __v: 0 }, { sort: {"polls.createdOn": -1} } function(err, docs) {
 		if (err) {
 			var err = new Error('Database query for GET "/" failed.');
 			err.status = 500; // internal server error
